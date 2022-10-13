087c029ba0b14fb71a202f292054407e05baec4d
William Gallo <williamgalloh@gmail.com>
2022-10-12 22:12:19 -0500

    Finished 'Meets Expectations' requirements
    Created an array of quote objects. After getting a random quote from the quotes array, the quote is displayed on the page with all its attributes.

--- js/script.js
+++ js/script.js
@@ -3,27 +3,81 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
- * `quotes` array 
-***/
+// Initialize `quotes` array 
+let quotes = [
+  {
+    quote: "The journey of a thousand miles begins with one step",
+    source: "Lao Tzu",
+    citation: "Twitter",
+    year: "2022"
+  },
+  {
+    quote: "Life is what happens when you’re busy making other plans",
+    source: "John Lennon",
+    citation: "Facebook",
+  },
+  {
+    quote: "You must be the change you wish to see in the world",
+    source: "Mahatma Gandhi",
+    year: "1987"
+  },
+  {
+    quote: "You only live once, but if you do it right, once is enough",
+    source: "Mae West"
+  },
+  {
+    quote: "Whether you think you can or you think you can’t, you’re right",
+    source: "Henry Ford"
+  }
+];
 
+/**
+ * getRandomQuote
+ * 
+ * Creates a random number, and uses that random number to return a random quote
+ *
+ * @returns {quote Object} A random quote object from the quotes array.
+ */
+function getRandomQuote() {
+  // Get a random number from 0 to the last index of the quotes array
+  let i = Math.floor(Math.random() * quotes.length);
 
+  // Use random number as index to select quote
+  let quote = quotes[i];
 
-/***
- * `getRandomQuote` function
-***/
+  return quote;
+}
 
+/**
+ * printQuote
+ * 
+ * Displays a new quote to the page
+ *
+ */
+function printQuote() {
+  // Get random quote
+  let quote = getRandomQuote();
 
+  // Build html to print
+  let html = `<p class="quote"> ${quote.quote} </p>
+    <p class="source"> ${quote.source}`;
 
-/***
- * `printQuote` function
-***/
+  // Check if there is a citation property
+  if(Object.keys(quote).indexOf('citation') > -1) {
+    html += `<span class="citation"> ${quote.citation} </span>`;
+  }
 
+  // Check if there is a year property
+  if(Object.keys(quote).indexOf('year') > -1) {
+    html += `<span class="year"> ${quote.year} </span>`;
+  }
 
+  html += `</p>`;
+
+  // Output quote to page
+  document.getElementById('quote-box').innerHTML = html; 
+}
 
 /***
  * click event listener for the print quote button
